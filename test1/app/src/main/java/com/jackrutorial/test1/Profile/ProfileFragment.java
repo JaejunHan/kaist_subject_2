baf8306479b7ce0c5e3ff093e9c1da8c29adba84
yeeun <yeeeun910@gmail.com>
2022-07-12 04:45:38 +0900

    집가기전 마지막 커밋

--- test1/app/src/main/java/com/jackrutorial/test1/Profile/ProfileFragment.java
+++ test1/app/src/main/java/com/jackrutorial/test1/Profile/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.jackrutorial.test1.Profile;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +8,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.jackrutorial.test1.R;
 
 
 public class ProfileFragment extends Fragment {
+
+    private String localhost = "https://9504-192-249-18-214.jp.ngrok.io";
+    View view;
+    private Context context;
+    // view 변수들
+    TextView profile_name, profile_comment;
+    TextView profile_career1,profile_career2,profile_career3, profile_group1, profile_group2, profile_group3;
+    /////////// textview
+
+    // bundle로 받아올 값들
+     String nickname;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -20,6 +33,9 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Bundle bundle = getArguments(); // detailPostFrag 에서 넘겨준 bundle 받아옴 (user name)
+        nickname = bundle.getString("nickname");
     }
 
     @Override
@@ -27,6 +43,8 @@ public class ProfileFragment extends Fragment {
                              ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_detail_post, container, false);
+        context = container.getContext();
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
