10cfb9e144113e46b67ba7d600e58548f774c5f1
JajunHan <hanjj03@naver.com>
2022-07-11 12:19:17 +0900

    채팅리스트 임시

---
+++ test1/app/src/main/java/com/jackrutorial/test1/chat_list_shown.java
@@ -0,0 +1,15 @@
+package com.jackrutorial.test1;
+
+public class chat_list_shown {
+    private String nickname = "";
+    private String send_date = "";
+    private String last_message = "";
+
+    public String getNickname() {return nickname;}
+    public String getSend_date() {return send_date;}
+    public String getLast_message() {return last_message;}
+
+    public void setNickname(String args) {nickname = args;}
+    public void setSend_date(String args) {send_date = args;}
+    public void setLast_message(String args) {last_message = args;}
+}
